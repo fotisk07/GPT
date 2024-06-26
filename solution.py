62a21d751363c8d8dc31e82b17e3545dafb4a780
Fotis <fotiskapotos@gmail.com>
2024-06-26 16:20:27 +0200

    Sample from the model

--- solution.py
+++ solution.py
@@ -1,71 +1,75 @@
+import math
+from dataclasses import dataclass
 import torch
 import torch.nn as nn
-from dataclasses import dataclass
-import tiktoken 
-import torch.nn.functional as F
-import math
+from torch.nn import functional as F
+import tiktoken
 
+# -----------------------------------------------------------------------------
 
-class MultiHeadAttention(nn.Module):
-    def __init__(self,config):
-        super(MultiHeadAttention, self).__init__()
-        assert config.n_embd % config.n_head == 0
-        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
+class CausalSelfAttention(nn.Module):
 
-        self.c_proj = nn.Linear(config.n_embd, config.n_embd)
-
-        self.nb_head = config.n_head
-        self.n_embd = config.n_embd
-
-        self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size, dtype=torch.bool))
-                                     .view(1, 1, config.block_size, config.block_size))
-    def forward(self, x):
-        # x: B, T, C
-        B, T, C = x.size()
-        qkv = self.c_attn(x)  # B, T, 3 * n_embd
-        q, k, v = torch.chunk(qkv, 3, dim=-1) # B, T, n_embd
-
-        q = q.view(B, T, self.nb_head, self.n_embd // self.nb_head).permute(0, 2, 1, 3) # B, nb_head, T, headsize
-        k = k.view(B, T, self.nb_head, self.n_embd // self.nb_head).permute(0, 2, 1, 3) # B, nb_head, T, headsize
-        v = v.view(B, T, self.nb_head, self.n_embd // self.nb_head).permute(0, 2, 1, 3) # B, nb_head, T, headsize
-
-        attention = (q @ k.transpose(-2, -1)) * (1.0 / torch.sqrt(k.size(-1)))
-        attention =  attention.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
-        attention = torch.nn.functional.softmax(attention, dim=-1) # B, nb_head, T, T
-        out = torch.matmul(attention, v) # B, nb_head, T, headsize
-        out = out.permute(0, 2, 1, 3).contiguous().view(B, T, self.n_embd) # B, T, n_embd
-        out = self.c_proj(out) # B, T, n_embd
-        
-        return out
-    
-class MLP(nn.Module):
     def __init__(self, config):
-        super(MLP, self).__init__()
-        self.c_fc = nn.Linear(config.n_embd, config.n_embd * 4)
-        self.act = nn.GELU(approximate='tanh')
-        self.c_proj = nn.Linear(config.n_embd * 4, config.n_embd)
+        super().__init__()
+        assert config.n_embd % config.n_head == 0
+        # key, query, value projections for all heads, but in a batch
+        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
+        # output projection
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        # regularization
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+        # not really a 'bias', more of a mask, but following the OpenAI/HF naming though
+        self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
+                                     .view(1, 1, config.block_size, config.block_size))
+
+    def forward(self, x):
+        B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
+        # calculate query, key, values for all heads in batch and move head forward to be the batch dim
+        # nh is "number of heads", hs is "head size", and C (number of channels) = nh * hs
+        # e.g. in GPT-2 (124M), n_head=12, hs=64, so nh*hs=C=768 channels in the Transformer
+        qkv = self.c_attn(x)
+        q, k, v = qkv.split(self.n_embd, dim=2)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        # attention (materializes the large (T,T) matrix for all the queries and keys)
+        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+        att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
+        att = F.softmax(att, dim=-1)
+        y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+        # output projection
+        y = self.c_proj(y)
+        return y
+
+class MLP(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc    = nn.Linear(config.n_embd, 4 * config.n_embd)
+        self.gelu    = nn.GELU(approximate='tanh')
+        self.c_proj  = nn.Linear(4 * config.n_embd, config.n_embd)
 
     def forward(self, x):
         x = self.c_fc(x)
-        x = self.act(x)
+        x = self.gelu(x)
         x = self.c_proj(x)
-
-        return x    
+        return x
 
 class Block(nn.Module):
+
     def __init__(self, config):
-        super(Block, self).__init__()
+        super().__init__()
         self.ln_1 = nn.LayerNorm(config.n_embd)
-        self.attn = MultiHeadAttention(config)
+        self.attn = CausalSelfAttention(config)
         self.ln_2 = nn.LayerNorm(config.n_embd)
         self.mlp = MLP(config)
-        
+
     def forward(self, x):
         x = x + self.attn(self.ln_1(x))
         x = x + self.mlp(self.ln_2(x))
         return x
-    
-
 
 @dataclass
 class GPTConfig:
@@ -75,36 +79,37 @@ class GPTConfig:
     n_head: int = 12 # number of heads
     n_embd: int = 768 # embedding dimension
 
-
 class GPT(nn.Module):
+
     def __init__(self, config):
-        super(GPT, self).__init__()
+        super().__init__()
+        self.config = config
+
         self.transformer = nn.ModuleDict(dict(
             wte = nn.Embedding(config.vocab_size, config.n_embd),
             wpe = nn.Embedding(config.block_size, config.n_embd),
             h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
             ln_f = nn.LayerNorm(config.n_embd),
         ))
-
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
-
-    def forward(self, x):
-        # x is (B, T)
-        B, T = x.size()
-        assert T <= self.transformer['wpe'].weight.size(0), "Cannot forward, model has been trained with T=%d, but current sequence has length T=%d" % (self.transformer['wpe'].weight.size(0), T)
-        token_encoding = self.transformer['wte'](x) # (B, T, C)
-        position_encoding = self.transformer['wpe'](torch.arange(x.size(1),dtype=torch.long, device=x.device)) # (T, C)
-
-        x = token_encoding + position_encoding # (B, T, C) broadcast along B
-
-        for block in self.transformer['h']:
+        
+    def forward(self, idx):
+        # idx is of shape (B, T)
+        B, T = idx.size()
+        assert T <= self.config.block_size, f"Cannot forward sequence of length {T}, block size is only {self.config.block_size}"
+        # forward the token and posisition embeddings
+        pos = torch.arange(0, T, dtype=torch.long, device=idx.device) # shape (T)
+        pos_emb = self.transformer.wpe(pos) # position embeddings of shape (T, n_embd)
+        tok_emb = self.transformer.wte(idx) # token embeddings of shape (B, T, n_embd)
+        x = tok_emb + pos_emb
+        # forward the blocks of the transformer
+        for block in self.transformer.h:
             x = block(x)
-
-        x = self.transformer['ln_f'](x)
-        logits = self.lm_head(x) # (B, T, V)
-
+        # forward the final layernorm and the classifier
+        x = self.transformer.ln_f(x)
+        logits = self.lm_head(x) # (B, T, vocab_size)
         return logits
-    
+
     @classmethod
     def from_pretrained(cls, model_type):
         """Loads pretrained GPT-2 model weights from huggingface"""
@@ -127,7 +132,6 @@ class GPT(nn.Module):
         sd = model.state_dict()
         sd_keys = sd.keys()
         sd_keys = [k for k in sd_keys if not k.endswith('.attn.bias')] # discard this mask / buffer, not a param
-        sd_keys = [k for k in sd_keys if not k.endswith('.attn.masked_bias')] # discard this mask / buffer, not a param
 
         # init a huggingface/transformers model
         model_hf = GPT2LMHeadModel.from_pretrained(model_type)
@@ -149,12 +153,13 @@ class GPT(nn.Module):
                     sd[k].copy_(sd_hf[k].t())
             else:
                 # vanilla copy over the other parameters
-                assert sd_hf[k].shape == sd[k].shape, f"mismatched shape: {sd_hf[k].shape} != {sd[k].shape} for key {k}"
+                assert sd_hf[k].shape == sd[k].shape
                 with torch.no_grad():
                     sd[k].copy_(sd_hf[k])
 
         return model
-    
+
+# -----------------------------------------------------------------------------
 
 if __name__ == "__main__":
     num_return_sequences = 5
@@ -162,6 +167,7 @@ if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     model = GPT.from_pretrained('gpt2')
+    print("didn't crash yay!")
     model.eval()
     model.to(device)
 
@@ -200,13 +206,3 @@ if __name__ == "__main__":
         tokens = x[i, :max_length].tolist()
         decoded = enc.decode(tokens)
         print(">", decoded)
-
-
-        
-
-        
-
-
-
-
-        
